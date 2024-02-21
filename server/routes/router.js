0bd803305a6960f1a38d0d9b4ab0df7adf097707
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2024-02-21 16:03:05 +1100

    connecting to postgresql db

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db");
 
 router.get("/test", (req, res) => {
   const testData = [
@@ -14,6 +15,20 @@ router.get("/test", (req, res) => {
   ];
 
   res.send(testData);
+});
+
+// Create a todo
+router.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 module.exports = router;
