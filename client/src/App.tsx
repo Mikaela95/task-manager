0be5e8f6f126ae98e00f4d170a919461270bc7eb
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2024-02-21 17:10:24 +1100

    creation APIs - CRUD

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, GridItem, Heading, Center } from "@chakra-ui/react";
 import axios from "axios";
 
